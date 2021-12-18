872e837cae6a2934119b3e930a32fe7088aefbdd
Alena Surovyatkina <aasurovyatkina@edu.hse.ru>
2021-12-19 00:39:10 +0300

    good tests and project ;)

--- test_game.py
+++ test_game.py
@@ -2,17 +2,19 @@ import math
 import random
 import pygame
 import pytest
-from main import fcnmath, tow_ok, tower, do_r, check, checkpoin
+import unittest
+from unittest import TestCase
+from main import fcnmath, tow_ok, tower, do_r, check, checkpoin, put_e
 
 
 def test_fcnmath():
     x1, x2, y1, y2 = 0, 1, 0, 1
     x_v = 1
     y_v = 1
-    ny_v1 = 0.7071067811865475
-    d1 = 1.4142135623730951
-    a1 = 0.7853981633974483
-    nx_v1 = 0.7071067811865475
+    ny_v1 = 1 / math.sqrt(2)
+    d1 = math.sqrt(2)
+    a1 = math.atan2(1, 1)
+    nx_v1 = 1 / math.sqrt(2)
     nx_v, ny_v, d, a = fcnmath(x1, x2, y1, y2)
     assert nx_v1 == nx_v and ny_v1 == ny_v and d1 == d and a1 == a
     "Should be 0.7071067811865475, 1.4142135623730951, 0.7853981633974483, 0.7071067811865475"
@@ -24,7 +26,7 @@ def test_fcnmath_eg():
     y_v = 3
     ny_v1 = 0.6
     d1 = 5
-    a1 = 0.6435011087932844
+    a1 = math.atan2(3,4)
     nx_v1 = 0.8
     nx_v, ny_v, d, a = fcnmath(x1, x2, y1, y2)
     assert nx_v1 == nx_v and ny_v1 == ny_v and d1 == d and a1 == a
@@ -49,9 +51,9 @@ def test_fcnmath_12():
     x_v = -12
     y_v = -5
     d1 = 13
-    nx_v1 = -0.9230769230769231
-    ny_v1 = -0.38461538461538464
-    a1 = -2.746801533890032
+    nx_v1 = -12 / 13
+    ny_v1 = -5 / 13
+    a1 = math.atan2(ny_v1, nx_v1)
     nx_v, ny_v, d, a = fcnmath(x1, x2, y1, y2)
     assert nx_v1 == nx_v and ny_v1 == ny_v and d1 == d and a1 == a
     "Should be 0.9230769230769231, 0.38461538461538464, 13, 0.3947911196997615"
@@ -157,6 +159,25 @@ def test_tow_ok3():
     road = do_r()
 
     assert tow_ok(x, y) == True
+
+
+class Test(TestCase):
+    def get_road(self):
+        # протяженность дороги
+        len_road = 38
+        self.assertEqual(len(do_r()), len_road)
+
+    def get_checkoints(self):
+        # количество контрольных точек
+        num_turns = 6
+        self.assertEqual(len(checkpoin()), num_turns)
+
+    def get_enem(self):
+        # количество врагов
+        num_enem = 6
+        red = (255, 0, 0)
+
+        self.assertEqual(len(put_e(10, red, 10, 3)), num_enem)
 
 
 def test_checkpoin():
